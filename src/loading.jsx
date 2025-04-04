c7f76ffe9a2d31744618f5fde794342a08c9e7bf
naveendoddi <prasadnaveen847@gmail.com>
2025-04-04 23:16:48 +0530

    pushing error, loading pages

--- src/loading.jsx
+++ src/loading.jsx
@@ -4,7 +4,7 @@ function Loading({ message = "Loading..." }) {
   return (
     <div className="d-flex justify-content-center mt-3 bg-light">
       <div className="text-center">
-        <div className="spinner-border text-yellow mb-3" role="status">
+        <div className="spinner-border text-warning mb-3" role="status">
           <span className="visually-hidden">{message}</span>
         </div>
         <div className="fw-semibold text-muted">{message}</div>

bd5932190cca9f126c434318f8e7eaff2a966a6c
naveendoddi <prasadnaveen847@gmail.com>
2025-04-05 10:08:55 +0530

    pushing map section

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -31,7 +31,7 @@ function Home() {
                   
                   <MapSection />
 
-                  <HomePageINC />
+                  {/* <HomePageINC /> */}
                   
                   <Footer />
 
